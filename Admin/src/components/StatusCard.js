fb6b2b6e0e1d2e360de6988d5ac1810c6d2a20ba
audyisnar <89682057+audyisnar@users.noreply.github.com>
2021-10-25 09:31:43 +0700

    Update StatusCard.js

--- Admin/src/components/StatusCard.js
+++ Admin/src/components/StatusCard.js
@@ -1,39 +1,75 @@
-import Card from '@material-tailwind/react/Card';
-import CardRow from '@material-tailwind/react/CardRow';
-import CardHeader from '@material-tailwind/react/CardHeader';
-import CardStatus from '@material-tailwind/react/CardStatus';
-import CardStatusFooter from '@material-tailwind/react/CardStatusFooter';
+import React from 'react';
 import Icon from '@material-tailwind/react/Icon';
 
-export default function StatusCard({
-    color,
-    icon,
-    title,
-    amount,
-    percentage,
-    percentageColor,
-    percentageIcon,
-    date,
-}) {
+export default function StatusCard() {
     return (
-        <div className="px-4 mb-10">
-            <Card>
-                <CardRow>
-                    <CardHeader color={color} iconOnly className="mb-0">
-                        <Icon name={icon} size="3xl" color="white" />
-                    </CardHeader>
-
-                    <CardStatus title={title} amount={amount} />
-                </CardRow>
-
-                <CardStatusFooter
-                    amount={percentage}
-                    color={percentageColor}
-                    date={date}
-                >
-                    <Icon color={percentageColor} name={percentageIcon} />
-                </CardStatusFooter>
-            </Card>
-        </div>
+        <>
+            {/* Card stats */}
+            <div className="flex flex-wrap">
+                <div className="w-full xl:w-4/12 px-4">
+                    <div className="relative flex flex-col min-w-0 break-words bg-white rounded-lg mb-6 xl:mb-0 shadow-lg">
+                        <div className="flex-auto p-6">
+                            <div className="flex flex-wrap">
+                                <div className="relative w-full pr-4 max-w-full flex-grow flex-1 mt-2 ml-2">
+                                    <h5 className="text-blueGray-400 uppercase text-xs mb-1">
+                                        Pengguna
+                                    </h5>
+                                    <span className="font-semibold text-2xl text-blueGray-700">
+                                        2.356
+                                    </span>
+                                </div>
+                                <div className="relative w-auto pl-4 flex-initial">
+                                    <div className="text-white p-3 text-center inline-flex items-center justify-center w-16 h-16 shadow-lg rounded-full bg-pink-500">
+                                        <Icon name="groups" size="3xl"/>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div className="w-full xl:w-4/12 px-4">
+                    <div className="relative flex flex-col min-w-0 break-words bg-white rounded-lg mb-6 xl:mb-0 shadow-lg">
+                        <div className="flex-auto p-6">
+                            <div className="flex flex-wrap">
+                                <div className="relative w-full pr-4 max-w-full flex-grow flex-1 mt-2 ml-2">
+                                    <h5 className="text-blueGray-400 uppercase text-xs mb-1">
+                                        Data Kalori
+                                    </h5>
+                                    <span className="font-semibold text-2xl text-blueGray-700">
+                                        1.100
+                                    </span>
+                                </div>
+                                <div className="relative w-auto pl-4 flex-initial">
+                                <div className="text-white p-3 text-center inline-flex items-center justify-center w-16 h-16 shadow-lg rounded-full bg-orange-500">
+                                        <Icon name="folder" size="3xl"/>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div className="w-full xl:w-4/12 px-4">
+                    <div className="relative flex flex-col min-w-0 break-words bg-white rounded-lg mb-6 xl:mb-0 shadow-lg">
+                        <div className="flex-auto p-6">
+                            <div className="flex flex-wrap">
+                                <div className="relative w-full pr-4 max-w-full flex-grow flex-1 mt-2 ml-2">
+                                    <h5 className="text-blueGray-400 uppercase text-xs mb-1">
+                                        Data Resep
+                                    </h5>
+                                    <span className="font-semibold text-2xl text-blueGray-700">
+                                        456
+                                    </span>
+                                </div>
+                                <div className="relative w-auto pl-4 flex-initial">
+                                <div className="text-white p-3 text-center inline-flex items-center justify-center w-16 h-16 shadow-lg rounded-full bg-blue-500">
+                                        <Icon name="book" size="3xl"/>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </>
     );
 }
