ea157c8c30c9ea6b6cd8fbfb1211f41a2e48bb23
audyisnar <89682057+audyisnar@users.noreply.github.com>
2021-10-25 09:29:32 +0700

    Update Dashboard.js

--- Admin/src/pages/Dashboard.js
+++ Admin/src/pages/Dashboard.js
@@ -9,50 +9,9 @@ export default function Dashboard() {
         <>
             <Sidebar />
             <div className="md:ml-64">
-                <div className="bg-primary500 pt-14 pb-28 px-3 md:px-8 h-auto">
+                <div className="bg-primary500 pt-8 pb-44 px-3 md:px-8 h-auto">
                     <div className="container mx-auto max-w-full">
-                        <div className="grid grid-cols-1 lg:grid-cols-2 xl:grid-cols-4">
-                                <StatusCard
-                                    color="pink"
-                                    icon="groups"
-                                    title="Pengguna"
-                                    amount="2.350"
-                                    percentage="15"
-                                    percentageIcon="arrow_upward"
-                                    percentageColor="green"
-                                    date="Hari ini"
-                                />
-                                <StatusCard
-                                    color="orange"
-                                    icon="paid"
-                                    title="Pendapatan"
-                                    amount="600.000"
-                                    percentage="250.000"
-                                    percentageIcon="arrow_upward"
-                                    percentageColor="green"
-                                    date="Hari ini"
-                                />
-                                <StatusCard
-                                    color="purple"
-                                    icon="folder"
-                                    title="Data Kalori"
-                                    amount="1.100"
-                                    percentage="35"
-                                    percentageIcon="arrow_upward"
-                                    percentageColor="green"
-                                    date="Hari ini"
-                                />
-                                <StatusCard
-                                    color="blue"
-                                    icon="poll"
-                                    title="Data Resep"
-                                    amount="456"
-                                    percentage="3"
-                                    percentageIcon="arrow_downward"
-                                    percentageColor="red"
-                                    date="Hari ini"
-                                />
-                        </div>
+                        <StatusCard />
                     </div>
                 </div>
 
