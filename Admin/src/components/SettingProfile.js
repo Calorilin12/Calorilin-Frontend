9a8e82f79240741ad026ce5839ef41d6c1cbcb59
audyisnar <audyistanianarita@gmail.com>
2021-12-06 13:04:15 +0700

    Fitur Ubah Password Admin

--- Admin/src/components/SettingProfile.js
+++ Admin/src/components/SettingProfile.js
@@ -1,6 +1,6 @@
 import React, { useEffect, useState, useRef } from 'react';
 import {Link, useParams, useHistory} from 'react-router-dom';
-import { USERS } from 'utils/url';
+import { PASSWORD } from 'utils/url';
 import { getToken } from 'utils/auth';
 import showIcon from '../assets/img/eye.png';
 import showOffIcon from '../assets/img/eye_invisible.png';
@@ -8,53 +8,65 @@ import axios from 'axios';
 
 const SettingProfile = () => {
     const { id } = useParams();
-    const [apiData, setApiData] = useState([]);
+    const [errorMatching, setErrorMatching] = useState(false);
+    const [error, setError] = useState(false);
     const [sandi, setSandi] = useState("");
+    const [sandiLama, setSandiLama] = useState("");
     const [konfirmasiSandi, setKonfirmasiSandi] = useState("");
-    const [name, setName] = useState("");
-    const [email, setEmail] = useState("");
     const [isRevealPwd, setIsRevealPwd] = useState(false);
+    const [isRevealPwdNew, setIsRevealPwdNew] = useState(false);
     const [isRevealConfirmPwd, setIsRevealConfirmPwd] = useState(false);
     const history = useHistory();
     const config = {
         headers: { Authorization: `Bearer ${getToken()}` }
     };
-    useEffect(() => {
-        axios
-            .get(USERS + id, config)
-            .then((res) => {
-                console.log(res);
-                setName(res.data.name);
-                setEmail(res.data.email);
-                setApiData(res.data);
-            })
-            .catch((err) => {
-                console.log(err);
-            }); 
-    }, []);
+    React.useEffect(() => {
+        if (sandiLama || sandi || konfirmasiSandi) {
+            setError(false);
+            setErrorMatching(false);
+        }
+        return () => {};
+    }, [sandiLama, sandi, konfirmasiSandi]);
+    // useEffect(() => {
+    //     axios
+    //         .get(PASSWORD + id, config)
+    //         .then((res) => {
+    //             console.log(res);
+    //             setName(res.data.name);
+    //             setEmail(res.data.email);
+    //             setApiData(res.data);
+    //         })
+    //         .catch((err) => {
+    //             console.log(err);
+    //         }); 
+    // }, []);
     function uploadData(){
-        if(!sandi || !konfirmasiSandi){
+        if(!sandiLama || !sandi || !konfirmasiSandi){
             alert('Ubah Password Gagal, Isi Field Terlebih Dahulu!');
         } else{
             if(sandi === konfirmasiSandi) {
                 console.log("Passwords match");
                 let formData = new FormData();
+                formData.append("passwordLama", sandiLama);
                 formData.append("password", sandi);
-                formData.append("name", name);
-                formData.append("email", email);
                 axios
-                .post(USERS + id + '?_method=PUT', formData, config)
+                .post(PASSWORD + id + '?_method=PUT', formData, config)
                 .then((res) => {
                     console.log(res.data);
-                    console.log(sandi)
+                    console.log(sandiLama);
+                    console.log(sandi);
                     console.log("sukses ubah sandi");
                     alert('Password Berhasil Dirubah');
                     history.replace('/profile-admin/1');
                 }).catch((err) => {
+                    //alert('Password Password Lama Tidak Sesuai');
+                    setError(true);
                     console.log(err);
+                    history.replace('/ubah-kata-sandi/1');
                 });
             } else {
-                alert("Passwords are not matching. Passwords should be same");
+                setErrorMatching(true);
+                //alert("Passwords are not matching. Passwords should be same");
             }
         }
     }
@@ -66,15 +78,27 @@ const SettingProfile = () => {
                     <div className="w-full p-4 px-5 py-5">
                         <h3 className="text-center text-2xl font-semibold mb-8">UBAH PASSWORD</h3>
                         <div className="mt-6">
-                            <label className="text-secondary500 text-sm">Password Baru</label><br/>
+                        <label className="text-secondary500 text-sm">Password Lama</label><br/>
                             <div className="inputBox">
-                                <input className="p-3 w-full h-10 rounded pl-4 mt-1 text-sm border focus:outline-none focus:border-gray-500" name="sandi" 
-                                        placeholder="Password Baru" type={isRevealPwd ? "text" : "password"} value={sandi} onChange={e => setSandi(e.target.value)}/>
+                                <input className="p-3 w-full h-10 rounded pl-4 mt-1 text-sm border focus:outline-none focus:border-gray-500" name="sandiLama" 
+                                        placeholder="Password Lama" type={isRevealPwd ? "text" : "password"} value={sandiLama} onChange={e => setSandiLama(e.target.value)}/>
                                 <div id="toggle">
                                     <img
                                         alt={isRevealPwd ? "Hide password" : "Show password"}
                                         src={isRevealPwd ? showOffIcon : showIcon}
                                         onClick={() => setIsRevealPwd(prevState => !prevState)}
+                                    />
+                                </div>
+                            </div><br/>
+                            <label className="text-secondary500 text-sm">Password Baru</label><br/>
+                            <div className="inputBox">
+                                <input className="p-3 w-full h-10 rounded pl-4 mt-1 text-sm border focus:outline-none focus:border-gray-500" name="sandi" 
+                                        placeholder="Password Baru" type={isRevealPwdNew ? "text" : "password"} value={sandi} onChange={e => setSandi(e.target.value)}/>
+                                <div id="toggle">
+                                    <img
+                                        alt={isRevealPwdNew ? "Hide password" : "Show password"}
+                                        src={isRevealPwdNew ? showOffIcon : showIcon}
+                                        onClick={() => setIsRevealPwdNew(prevState => !prevState)}
                                     />
                                 </div>
                             </div>
@@ -95,6 +119,18 @@ const SettingProfile = () => {
                             <input type="password" className="p-3 w-full h-10 rounded pl-4 mt-1 text-sm border focus:outline-none focus:border-gray-500" 
                                     placeholder="Password Baru" value={sandi} onChange={(e) => setSandi(e.target.value)}
                             /> */}
+                            {errorMatching && 
+                                <div class="bg-red-100 border border-red-400 text-red-700 px-4 py-2 rounded relative mt-4" role="alert">
+                                    <strong class="font-bold text-sm">Ubah Password Gagal! </strong>
+                                    <span class="block sm:inline text-sm">Password Baru dan Konfirmasi Password Tidak Sama</span>
+                                </div>
+                            }
+                            {error && 
+                                <div class="bg-red-100 border border-red-400 text-red-700 px-4 py-2 rounded relative mt-4" role="alert">
+                                    <strong class="font-bold text-sm">Ubah Password Gagal! </strong>
+                                    <span class="block sm:inline text-sm">Password Lama Tidak Sesuai</span>
+                                </div>
+                            }
                         </div>
                         <Link
                             onClick={uploadData}
--- Admin/src/utils/url.js
+++ Admin/src/utils/url.js
@@ -4,6 +4,7 @@ export const BASE_URL = 'http://192.168.194.60:8000';
 
 export const LOGIN = `${BASE_URL}/api/login`;
 export const USERS = `${BASE_URL}/api/users/`;
+export const PASSWORD = `${BASE_URL}/api/users_password/`;
 export const DASHBOARD = `${BASE_URL}/api/dashboard`;
 export const RECIPES = `${BASE_URL}/api/recipes/`;
 export const FOOD_MATERIALS = `${BASE_URL}/api/food-materials/`;
