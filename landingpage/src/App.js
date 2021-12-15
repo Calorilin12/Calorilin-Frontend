e5ec170e973de67b30b38575c143b34fb911f946
stafanusirgy2202 <stefanusirgy2202@gmail.com>
2021-12-15 15:30:06 +0700

    menambahkan bagian wrapper, tambahan meet our team, dan footer

--- landingpage/src/App.js
+++ landingpage/src/App.js
@@ -1,6 +1,7 @@
 import './App.css';
 import mockup from './assets/img/Frame 82.png';
-import illustrator from './assets/img/pana.png';
+import illustrator from './assets/img/Kontrol Kalori.svg';
+import illustrator2 from './assets/img/Control Food.svg';
 import irgy from './assets/img/irgy.png';
 import rahano from './assets/img/rahano.png';
 import audy from './assets/img/audy.png';
@@ -24,6 +25,8 @@ function App() {
       {/* <!-- showcase --> */}
       <div className="showcase">
         <div className="showcase-container">
+
+          {/* left showcase */}
           <div className="left-showcase">
             <h1 className="showcase-head">Calorilin</h1>
             <h1 className="showcase-title">Make your life is healthier</h1>
@@ -35,66 +38,236 @@ function App() {
               <a href="#" className="btn btn-started">Get started</a>
             </div>
           </div>
+
+          {/* right showcase */}
           <div className="right-showcase">
             <img src={mockup} alt="" />
           </div>
+
         </div>
       </div>
+
       {/* <!-- rating section --> */}
       <div className="rating-section">
         <div className="header">
-          <h1>Apa saja yang tersedia dalam <span className="span">Calorilin ??</span></h1>
+          <h2>Apa saja yang tersedia dalam <span className="span">Calorilin ??</span></h2>
         </div>
         <div className="row1-container">
           <div className="box box-down cyan">
             <h2>Kontrol Kalori</h2>
-            <p>Memberikan kemudahan akses materi belajar iot</p>
+            <p>Layanan kontrol kalori harian hanya dengan input bahan makanan</p>
           </div>
 
           <div className="box red">
             <h2>Rekomendasi Makanan</h2>
-            <p>Memberikan materi yang iot yang cukup lengkap</p>
+            <p>Memberikan rekomendasi makanan yang sesuai dengan kebutuhan tubuh</p>
           </div>
 
           <div className="box box-down blue">
             <h2>Informasi Kalori</h2>
-            <p>Memberikan akses materi belajar gratis</p>
+            <p>Database kalori makanan terlengkap dengan jumlah data 1000+</p>
           </div>
         </div>
         <div className="row2-container">
           <div className="box orange">
             <h2>Tutorial Resep Makanan</h2>
-            <p>Memberikan materi belajar dengan update terbaru</p>
+            <p>Tutorial pembuatan makanan yang terjamin kelezatan dan kesehatannya</p>
           </div>
         </div>
       </div>
-      {/* <!-- article section --> */}
+
+
+      {/* <!-- info section --> */}
       <div className="section-info">
         <div className="info-container">
           <div className="left-info">
-            <h1 className="info-title">Pengalaman baru dalam mengkontrol kalori secara dinamis dan praktis untuk memulai gaya hidup sehat !</h1>
+            <h1 className="info-title">Pengalaman baru dalam <span>mengkontrol</span> kalori secara <span>dinamis dan praktis</span> untuk memulai gaya hidup sehat !</h1>
             <p className="info-description">Mengatur pola makan setiap harinya sesuai dengan kebutuhan kalori pengguna </p>
           </div>
           <div className="right-info">
             <img className="info-right" src={illustrator} alt="" />
           </div>
         </div>
+
+        <div className="info-container">
+          <div className="right-info">
+            <img className="info-right" src={illustrator2} alt="" />
+          </div>
+          <div className="left-info">
+            <h1 className="info-title">Panduan dalam <span>pemilihan makanan</span> yang cocok yang disesuaikan dengan <span>kondisi tubuh</span> pengguna !</h1>
+            <p className="info-description">Memberikan rekomendasi makanan sesuai dengan penyakit yang dialami pengguna </p>
+          </div>
+          
+        </div>
+
         <div className="card-info">
           <h2>Penasaran fitur dalam Aplikasi <span>Calorilin?</span></h2>
           <div className="card-box-info">
-            <div className="left-card-info">
-            <iframe width="420" height="250"
-                src="https://www.youtube.com/embed/tgbNymZ7vqY">
-            </iframe>
-            </div>
-            <div className="right-card-info">
-              <h4>Healthy food</h4>
-              <p>Membatu pengguna dalam pengelolaan pola makan untuk menjaga kesehatan pengguna</p>
-            </div>
+          {/* Iframe  */}
+          <div class="youtube">
+            <iframe class="responsive-iframe" src="https://www.youtube.com/embed/tgbNymZ7vqY"></iframe>
+          </div>
+
           </div>
         </div>
       </div>
-      {/* <!-- testimonial section -->
+      {/* <!-- info section -->
+
+      <!-- team section --> */}
+
+      <div className="wrapper-testi-title">
+        <div className="wrapper-testi">
+          <div class="container mt-100 mt-60">
+            <div class="row">
+                <div class="col-12 text-center">
+                    <div class="section-title">
+                        <h4 class="title mb-4">Our Creative Minds</h4>
+                        <p class="text-muted para-desc mx-auto mb-0">Build responsive, mobile-first projects on the web with the world's most popular front-end component library.</p>
+                    </div>
+                </div>
+            </div>
+
+            <div className="row">
+              <div className="col-lg-4 col-md-6 col-12 mt-4 pt-2">
+                  <div className="mt-4 pt-2">
+                      <div className="team position-relative d-block text-center">
+                          <div className="image position-relative d-block overflow-hidden">
+                              <img src={rahano} className="img-fluid rounded" alt=""></img>
+                              <div className="overlay rounded bg-dark"></div>
+                          </div>
+                          <div className="content py-2 member-position bg-white border-bottom overflow-hidden rounded d-inline-block">
+                              <h4 className="title mb-0">Calvin Carlo</h4>
+                              <small className="text-muted">Founder</small>
+                          </div>
+                          <ul className="list-unstyled team-social social-icon social mb-0">
+                              <li className="list-inline-item"><a href="javascript:void(0)" className="rounded"><i className="mdi mdi-facebook" title="Facebook"></i></a></li>
+                              <li className="list-inline-item"><a href="javascript:void(0)" className="rounded"><i className="mdi mdi-instagram" title="Instagram"></i></a></li>
+                              <li className="list-inline-item"><a href="javascript:void(0)" className="rounded"><i className="mdi mdi-twitter" title="Twitter"></i></a></li>
+                              <li className="list-inline-item"><a href="javascript:void(0)" className="rounded"><i className="mdi mdi-google-plus" title="Google +"></i></a></li>
+                              <li className="list-inline-item"><a href="javascript:void(0)" className="rounded"><i className="mdi mdi-linkedin" title="Linkedin"></i></a></li>
+                          </ul>
+                      </div>
+                  </div>
+              </div>
+              
+              <div className="col-lg-4 col-md-6 col-12 mt-4 pt-2">
+                  <div className="mt-4 pt-2">
+                      <div className="team position-relative d-block text-center">
+                          <div className="image position-relative d-block overflow-hidden">
+                              <img src={audy} class="img-fluid rounded" alt=""></img>
+                              <div className="overlay rounded bg-dark"></div>
+                          </div>
+                          <div className="content py-2 member-position bg-white border-bottom overflow-hidden rounded d-inline-block">
+                              <h4 className="title mb-0">Martin Sobhe</h4>
+                              <small className="text-muted">C.E.O.</small>
+                          </div>
+                          <ul className="list-unstyled team-social social-icon social mb-0">
+                              <li className="list-inline-item"><a href="javascript:void(0)" className="rounded"><i className="mdi mdi-facebook" title="Facebook"></i></a></li>
+                              <li className="list-inline-item"><a href="javascript:void(0)" className="rounded"><i className="mdi mdi-instagram" title="Instagram"></i></a></li>
+                              <li className="list-inline-item"><a href="javascript:void(0)" className="rounded"><i className="mdi mdi-twitter" title="Twitter"></i></a></li>
+                              <li className="list-inline-item"><a href="javascript:void(0)" className="rounded"><i className="mdi mdi-google-plus" title="Google +"></i></a></li>
+                              <li className="list-inline-item"><a href="javascript:void(0)" className="rounded"><i className="mdi mdi-linkedin" title="Linkedin"></i></a></li>
+                          </ul>
+                      </div>
+                  </div>
+              </div>
+              
+              <div className="col-lg-4 col-md-6 col-12 mt-4 pt-2">
+                  <div className="mt-4 pt-2">
+                      <div className="team position-relative d-block text-center">
+                          <div className="image position-relative d-block overflow-hidden">
+                              <img src={rahano} className="img-fluid rounded" alt=""></img>
+                              <div className="overlay rounded bg-dark"></div>
+                          </div>
+                          <div className="content py-2 member-position bg-white border-bottom overflow-hidden rounded d-inline-block">
+                              <h4 className="title mb-0">Bobby DE</h4>
+                              <small className="text-muted">Developer</small>
+                          </div>
+                          <ul className="list-unstyled team-social social-icon social mb-0">
+                              <li className="list-inline-item"><a href="javascript:void(0)" className="rounded"><i className="mdi mdi-facebook" title="Facebook"></i></a></li>
+                              <li className="list-inline-item"><a href="javascript:void(0)" className="rounded"><i className="mdi mdi-instagram" title="Instagram"></i></a></li>
+                              <li className="list-inline-item"><a href="javascript:void(0)" className="rounded"><i className="mdi mdi-twitter" title="Twitter"></i></a></li>
+                              <li className="list-inline-item"><a href="javascript:void(0)" className="rounded"><i className="mdi mdi-google-plus" title="Google +"></i></a></li>
+                              <li className="list-inline-item"><a href="javascript:void(0)" className="rounded"><i className="mdi mdi-linkedin" title="Linkedin"></i></a></li>
+                          </ul>
+                      </div>
+                  </div>
+              </div>
+
+              <div className="row">
+                <div className="col-lg-4 col-md-6 col-12 mt-4 pt-2">
+                    <div className="mt-4 pt-2">
+                        <div className="team position-relative d-block text-center">
+                            <div className="image position-relative d-block overflow-hidden">
+                                <img src={rahano} className="img-fluid rounded" alt=""></img>
+                                <div className="overlay rounded bg-dark"></div>
+                            </div>
+                            <div className="content py-2 member-position bg-white border-bottom overflow-hidden rounded d-inline-block">
+                                <h4 className="title mb-0">Calvin Carlo</h4>
+                                <small className="text-muted">Founder</small>
+                            </div>
+                            <ul className="list-unstyled team-social social-icon social mb-0">
+                                <li className="list-inline-item"><a href="javascript:void(0)" className="rounded"><i className="mdi mdi-facebook" title="Facebook"></i></a></li>
+                                <li className="list-inline-item"><a href="javascript:void(0)" className="rounded"><i className="mdi mdi-instagram" title="Instagram"></i></a></li>
+                                <li className="list-inline-item"><a href="javascript:void(0)" className="rounded"><i className="mdi mdi-twitter" title="Twitter"></i></a></li>
+                                <li className="list-inline-item"><a href="javascript:void(0)" className="rounded"><i className="mdi mdi-google-plus" title="Google +"></i></a></li>
+                                <li className="list-inline-item"><a href="javascript:void(0)" className="rounded"><i className="mdi mdi-linkedin" title="Linkedin"></i></a></li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+                
+                <div className="col-lg-4 col-md-6 col-12 mt-4 pt-2">
+                    <div className="mt-4 pt-2">
+                        <div className="team position-relative d-block text-center">
+                            <div className="image position-relative d-block overflow-hidden">
+                                <img src={audy} class="img-fluid rounded" alt=""></img>
+                                <div className="overlay rounded bg-dark"></div>
+                            </div>
+                            <div className="content py-2 member-position bg-white border-bottom overflow-hidden rounded d-inline-block">
+                                <h4 className="title mb-0">Martin Sobhe</h4>
+                                <small className="text-muted">C.E.O.</small>
+                            </div>
+                            <ul className="list-unstyled team-social social-icon social mb-0">
+                                <li className="list-inline-item"><a href="javascript:void(0)" className="rounded"><i className="mdi mdi-facebook" title="Facebook"></i></a></li>
+                                <li className="list-inline-item"><a href="javascript:void(0)" className="rounded"><i className="mdi mdi-instagram" title="Instagram"></i></a></li>
+                                <li className="list-inline-item"><a href="javascript:void(0)" className="rounded"><i className="mdi mdi-twitter" title="Twitter"></i></a></li>
+                                <li className="list-inline-item"><a href="javascript:void(0)" className="rounded"><i className="mdi mdi-google-plus" title="Google +"></i></a></li>
+                                <li className="list-inline-item"><a href="javascript:void(0)" className="rounded"><i className="mdi mdi-linkedin" title="Linkedin"></i></a></li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="col-lg-4 col-md-6 col-12 mt-4 pt-2">
+                  <div className="mt-4 pt-2">
+                      <div className="team position-relative d-block text-center">
+                          <div className="image position-relative d-block overflow-hidden">
+                              <img src={rahano} className="img-fluid rounded" alt=""></img>
+                              <div className="overlay rounded bg-dark"></div>
+                          </div>
+                          <div className="content py-2 member-position bg-white border-bottom overflow-hidden rounded d-inline-block">
+                              <h4 className="title mb-0">Cristino Murphy</h4>
+                              <small className="text-muted">Designer</small>
+                          </div>
+                          <ul className="list-unstyled team-social social-icon social mb-0">
+                              <li className="list-inline-item"><a href="javascript:void(0)" className="rounded"><i className="mdi mdi-facebook" title="Facebook"></i></a></li>
+                              <li className="list-inline-item"><a href="javascript:void(0)" className="rounded"><i className="mdi mdi-instagram" title="Instagram"></i></a></li>
+                              <li className="list-inline-item"><a href="javascript:void(0)" className="rounded"><i className="mdi mdi-twitter" title="Twitter"></i></a></li>
+                              <li className="list-inline-item"><a href="javascript:void(0)" className="rounded"><i className="mdi mdi-google-plus" title="Google +"></i></a></li>
+                              <li className="list-inline-item"><a href="javascript:void(0)" className="rounded"><i className="mdi mdi-linkedin" title="Linkedin"></i></a></li>
+                          </ul>
+                      </div>
+                  </div>
+              </div>
+              
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>      
+      {/* <!-- team section -->
+
       <!-- testimonial --> */}
       <h1 className="wrapper-testi-title">Apa kata mereka tentang <span>Calorilin ??</span></h1>
       <div className="wrapper-testi">
@@ -159,17 +332,25 @@ function App() {
           </div>
         </div>
       </div>
+
       {/* <!-- footer --> */}
-      <footer>
-        <div className="container">
-          {/* <!-- LEFT FOOTER --> */}
-          <div className="left-footer">
-            <h3 className="header">Calorilin</h3>
-            <p className="left-description">Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa.</p>
-            <p className="copyright">&copy;copyright 2021 All rights reserved</p>
-          </div>          
-        </div>
-      </footer>
+      <div class="footer-basic">
+        <footer>
+            <div class="social"><a href="#"><i class="fab fa-twitter"></i></a><a href="#"><i class="fab fa-instagram"></i></a><a href="#"><i class="fab fa-facebook"></i></a><a href="#"><i class="fab fa-github"></i></a></div>
+            <ul class="list-inline">
+                <li class="list-inline-item"><a href="#">Home</a></li>
+                <li class="list-inline-item"><a href="#">Services</a></li>
+                <li class="list-inline-item"><a href="#">About</a></li>
+                <li class="list-inline-item"><a href="#">Terms</a></li>
+                <li class="list-inline-item"><a href="#">Privacy Policy</a></li>
+            </ul>
+            <p class="copyright">Calorilin © 2021</p>
+        </footer>
+      </div>
+
+
+
+      
     </div>
     </div>
   );
