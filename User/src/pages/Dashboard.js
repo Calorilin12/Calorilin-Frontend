48fda18dec1fa8c886e60673951e91255606e153
stafanusirgy2202 <stefanusirgy2202@gmail.com>
2022-01-03 09:19:37 +0700

    menambahkan card untuk article di halaman user

--- User/src/pages/Dashboard.js
+++ User/src/pages/Dashboard.js
@@ -2,6 +2,8 @@ import React from 'react';
 import Sidebar from 'components/Sidebar';
 import Footer from 'components/Footer';
 import StatusCard from 'components/StatusCard';
+import article from '../assets/img/ayam.jpg';
+import placeholder from '../assets/img/placeholder.jpg';
 
 export default function Dashboard() {
     return (
@@ -13,6 +15,230 @@ export default function Dashboard() {
                         <StatusCard />
                     </div>
                 </div>
+                
+                <div className="container my-12 mx-auto px-4 md:px-12">
+                    <div className="flex flex-wrap -mx-1 lg:-mx-4">
+
+                        <div className="my-1 px-1 w-full md:w-1/2 lg:my-4 lg:px-4 lg:w-1/3">
+
+                            <article className="overflow-hidden rounded-lg shadow-lg">
+
+                                <a href="#">
+                                    <img src={article} className="block h-auto w-full" />
+                                </a>
+
+                                <header className="flex items-center justify-between leading-tight p-2 md:p-4">
+                                    <h1 className="text-lg">
+                                        <a className="no-underline hover:underline text-black" href="#">
+                                            Article Title
+                                        </a>
+                                    </h1>
+                                    <p className="text-grey-darker text-sm">
+                                        11/1/19
+                                    </p>
+                                </header>
+
+                                <footer className="flex items-center justify-between leading-none p-2 md:p-4">
+                                    <a className="flex items-center no-underline hover:underline text-black" href="#">
+                                        <img alt="Placeholder" className="block rounded-full" src={placeholder} />
+                                        <p className="ml-2 text-sm">
+                                            Author Name
+                                        </p>
+                                    </a>
+                                    <a className="no-underline text-grey-darker hover:text-red-dark" href="#">
+                                        <span className="hidden">Like</span>
+                                        <i className="fa fa-heart"></i>
+                                    </a>
+                                </footer>
+
+                            </article>
+
+                        </div>
+
+                        <div className="my-1 px-1 w-full md:w-1/2 lg:my-4 lg:px-4 lg:w-1/3">
+
+                            <article className="overflow-hidden rounded-lg shadow-lg">
+
+                                <a href="#">
+                                    <img alt="Placeholder" className="block h-auto w-full" src={article} />
+                                </a>
+
+                                <header className="flex items-center justify-between leading-tight p-2 md:p-4">
+                                    <h1 className="text-lg">
+                                        <a className="no-underline hover:underline text-black" href="#">
+                                            Article Title
+                                        </a>
+                                    </h1>
+                                    <p className="text-grey-darker text-sm">
+                                        11/1/19
+                                    </p>
+                                </header>
+
+                                <footer className="flex items-center justify-between leading-none p-2 md:p-4">
+                                    <a className="flex items-center no-underline hover:underline text-black" href="#">
+                                        <img alt="Placeholder" className="block rounded-full" src={placeholder} />
+                                        <p className="ml-2 text-sm">
+                                            Author Name
+                                        </p>
+                                    </a>
+                                    <a className="no-underline text-grey-darker hover:text-red-dark" href="#">
+                                        <span className="hidden">Like</span>
+                                        <i className="fa fa-heart"></i>
+                                    </a>
+                                </footer>
+
+                            </article>
+
+                        </div>
+
+                        <div className="my-1 px-1 w-full md:w-1/2 lg:my-4 lg:px-4 lg:w-1/3">
+
+                            <article className="overflow-hidden rounded-lg shadow-lg">
+
+                                <a href="#">
+                                    <img alt="Placeholder" className="block h-auto w-full" src={article} />
+                                </a>
+
+                                <header className="flex items-center justify-between leading-tight p-2 md:p-4">
+                                    <h1 className="text-lg">
+                                        <a className="no-underline hover:underline text-black" href="#">
+                                            Article Title
+                                        </a>
+                                    </h1>
+                                    <p className="text-grey-darker text-sm">
+                                        11/1/19
+                                    </p>
+                                </header>
+
+                                <footer className="flex items-center justify-between leading-none p-2 md:p-4">
+                                    <a className="flex items-center no-underline hover:underline text-black" href="#">
+                                        <img alt="Placeholder" className="block rounded-full" src={placeholder} />
+                                        <p className="ml-2 text-sm">
+                                            Author Name
+                                        </p>
+                                    </a>
+                                    <a className="no-underline text-grey-darker hover:text-red-dark" href="#">
+                                        <span className="hidden">Like</span>
+                                        <i className="fa fa-heart"></i>
+                                    </a>
+                                </footer>
+
+                            </article>
+
+                        </div>
+
+                        <div className="my-1 px-1 w-full md:w-1/2 lg:my-4 lg:px-4 lg:w-1/3">
+
+                            <article className="overflow-hidden rounded-lg shadow-lg">
+
+                                <a href="#">
+                                    <img alt="Placeholder" className="block h-auto w-full" src={article} />
+                                </a>
+
+                                <header className="flex items-center justify-between leading-tight p-2 md:p-4">
+                                    <h1 className="text-lg">
+                                        <a className="no-underline hover:underline text-black" href="#">
+                                            Article Title
+                                        </a>
+                                    </h1>
+                                    <p className="text-grey-darker text-sm">
+                                        11/1/19
+                                    </p>
+                                </header>
+
+                                <footer className="flex items-center justify-between leading-none p-2 md:p-4">
+                                    <a className="flex items-center no-underline hover:underline text-black" href="#">
+                                        <img alt="Placeholder" className="block rounded-full" src={placeholder} />
+                                        <p className="ml-2 text-sm">
+                                            Author Name
+                                        </p>
+                                    </a>
+                                    <a className="no-underline text-grey-darker hover:text-red-dark" href="#">
+                                        <span className="hidden">Like</span>
+                                        <i className="fa fa-heart"></i>
+                                    </a>
+                                </footer>
+
+                            </article>
+
+                        </div>
+
+                        <div className="my-1 px-1 w-full md:w-1/2 lg:my-4 lg:px-4 lg:w-1/3">
+
+                            <article className="overflow-hidden rounded-lg shadow-lg">
+
+                                <a href="#">
+                                    <img alt="Placeholder" className="block h-auto w-full" src={article} />
+                                </a>
+
+                                <header className="flex items-center justify-between leading-tight p-2 md:p-4">
+                                    <h1 className="text-lg">
+                                        <a className="no-underline hover:underline text-black" href="#">
+                                            Article Title
+                                        </a>
+                                    </h1>
+                                    <p className="text-grey-darker text-sm">
+                                        11/1/19
+                                    </p>
+                                </header>
+
+                                <footer className="flex items-center justify-between leading-none p-2 md:p-4">
+                                    <a className="flex items-center no-underline hover:underline text-black" href="#">
+                                        <img alt="Placeholder" className="block rounded-full" src={placeholder} />
+                                        <p className="ml-2 text-sm">
+                                            Author Name
+                                        </p>
+                                    </a>
+                                    <a className="no-underline text-grey-darker hover:text-red-dark" href="#">
+                                        <span className="hidden">Like</span>
+                                        <i className="fa fa-heart"></i>
+                                    </a>
+                                </footer>
+
+                            </article>
+
+                        </div>
+
+                        <div className="my-1 px-1 w-full md:w-1/2 lg:my-4 lg:px-4 lg:w-1/3">
+
+                            <article className="overflow-hidden rounded-lg shadow-lg">
+
+                                <a href="#">
+                                    <img alt="Placeholder" className="block h-auto w-full" src={article} />
+                                </a>
+
+                                <header className="flex items-center justify-between leading-tight p-2 md:p-4">
+                                    <h1 className="text-lg">
+                                        <a className="no-underline hover:underline text-black" href="#">
+                                            Article Title
+                                        </a>
+                                    </h1>
+                                    <p className="text-grey-darker text-sm">
+                                        11/1/19
+                                    </p>
+                                </header>
+
+                                <footer className="flex items-center justify-between leading-none p-2 md:p-4">
+                                    <a className="flex items-center no-underline hover:underline text-black" href="#">
+                                        <img alt="Placeholder" className="block rounded-full" src={placeholder} />
+                                        <p className="ml-2 text-sm">
+                                            Author Name
+                                        </p>
+                                    </a>
+                                    <a className="no-underline text-grey-darker hover:text-red-dark" href="#">
+                                        <span className="hidden">Like</span>
+                                        <i className="fa fa-heart"></i>
+                                    </a>
+                                </footer>
+
+                            </article>
+
+                        </div>
+
+                    </div>
+                </div>
+
+
             </div>
         </>
     );
